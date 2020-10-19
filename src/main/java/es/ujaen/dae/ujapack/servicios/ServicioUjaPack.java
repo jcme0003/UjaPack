62e7717a3afa3056195cc52cbdc740d075b63d21
Jose Carlos Mena <jcme0003@red.ujaen.es>
2020-10-19 15:42:00 +0200

    App basica de ejemplo

--- src/main/java/es/ujaen/dae/ujapack/servicios/ServicioUjaPack.java
+++ src/main/java/es/ujaen/dae/ujapack/servicios/ServicioUjaPack.java
@@ -3,12 +3,17 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package es.ujaen.dae.ujapack.app;
+package es.ujaen.dae.ujapack.servicios;
+
+import org.springframework.stereotype.Service;
 
 /**
  *
  * @author Root
  */
-public class UjaPack {
-    
+@Service
+public class ServicioUjaPack {
+    public void helloUja(){
+        System.out.println("Hello Uja");
+    }
 }
