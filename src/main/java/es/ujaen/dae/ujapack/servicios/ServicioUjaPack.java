c5c8ca216055432be2689ceecb74a17dec746340
Jose Carlos Mena <jcme0003@red.ujaen.es>
2020-10-20 19:34:17 +0200

    funcion cargajson() pero solo mostramos datos por pantalla. Falta crear centros logisticos, oficinas...

--- src/main/java/es/ujaen/dae/ujapack/servicios/ServicioUjaPack.java
+++ src/main/java/es/ujaen/dae/ujapack/servicios/ServicioUjaPack.java
@@ -9,9 +9,21 @@ import es.ujaen.dae.ujapack.entidades.puntocontrol.CentroLogistico;
 import es.ujaen.dae.ujapack.entidades.Cliente;
 import es.ujaen.dae.ujapack.entidades.Envio;
 import es.ujaen.dae.ujapack.objetosvalor.Paquete;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.TreeMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
 /**
@@ -38,6 +50,65 @@ public class ServicioUjaPack {
      */
     public void helloUja(){
         System.out.println("Hello Uja");
+    }
+    
+    /**
+     * Carga archivo JSon que contiene los centros logisticos, oficinas y conexiones
+     * @param url url donde esta guardado el fichero JSon
+     */
+    public void cargaJSon(String url){
+        try{
+            // Carga y almacena en un string el archivo json
+            File file = new File(url);
+            String data;
+            try (Scanner sc = new Scanner(new FileInputStream(file), "UTF-8")) {
+                data = new String();
+                while(sc.hasNextLine()){
+                    data += sc.nextLine();
+                }
+            }
+//            System.out.println(data);
+            
+            JSONObject obj = new JSONObject(data);
+            JSONObject elem;
+            JSONArray arr;
+            
+            String id;
+            String nombre;
+            String localizacion;
+            List<String> provincias = new ArrayList<>();
+            List<Integer> conexiones = new ArrayList<>();
+            
+            // Cargamos datos de cada identificador del Json "1", "2", ... "10"
+            for(int i = 1; i <= 10; i++){
+                id = Integer.toString(i);
+                elem = obj.getJSONObject(id);
+                
+                nombre = elem.getString("nombre");
+//                System.out.println(nombre);
+                
+                localizacion = elem.getString("localización");
+//                System.out.println(localizacion);
+                
+                // Cargamos las provincias correspondientes al identificador actual i
+                arr = elem.getJSONArray("provincias");
+                for(int j = 0; j < arr.length(); j++){
+                    provincias.add(arr.getString(j));
+//                    System.out.println(arr.getString(j));
+                }
+                
+                // Cargamos las conexiones correspondientes al identificador actual i
+                arr = elem.getJSONArray("conexiones");
+                for(int j = 0; j < arr.length(); j++){
+                    conexiones.add(arr.getInt(j));
+//                    System.out.println(arr.getInt(j));
+                }
+            }
+           
+        } catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+        
     }
     
     /**
