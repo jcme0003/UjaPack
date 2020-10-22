ce2d9f06045ef850ac651e6177c3f7a14c87b647
Ana <alag0006@red.ujaen.es>
2020-10-22 18:48:28 +0200

    Test creados

---
+++ src/test/java/es/ujaen/dae/ujapack/entidades/ClienteTest.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.ujaen.dae.ujapack.entidades;
+
+import java.util.Set;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+
+/**
+ *
+ * @author Ana
+ */
+public class ClienteTest {
+     public ClienteTest() {
+    }
+
+//    @Test
+    void testValidacionCliente() {
+ 
+        Cliente cliente = new Cliente(
+                "45789632M", 
+                "Jose",
+                "Bermudez Carvajal", 
+                "Calle La Luna",
+                "Jaén",
+                "953010203",
+                "jbc@gmail.com");
+               
+        
+        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+        Set<ConstraintViolation<Cliente>> violations = validator.validate(cliente);
+                
+//        Assertions.assertThat(violations).isEmpty();
+    }
+
+}
---
+++ src/test/java/es/ujaen/dae/ujapack/servicios/EnvioTest.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.ujaen.dae.ujapack.servicios;
+
+/**
+ *
+ * @author Ana
+ */
+public class EnvioTest {
+    public EnvioTest(){
+        
+    }
+    
+//    @Test
+    void testValidacionEnvio1(){
+        
+    }
+    
+//    @Test
+    void testValidacionEnvio2(){
+        
+    }
+    
+//    @Test
+    void testValidacionEnvio3(){
+        
+    }
+}
