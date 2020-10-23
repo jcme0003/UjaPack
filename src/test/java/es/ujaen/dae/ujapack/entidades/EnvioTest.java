c534f48d028ff3dafa4d03295cf95b6f9c782eb0
Jose Carlos Mena <jcme0003@red.ujaen.es>
2020-10-23 12:40:22 +0200

    Test CalculaImporteTipo1 y CalculaImporteTipo2 OK

--- src/test/java/es/ujaen/dae/ujapack/entidades/EnvioTest.java
+++ src/test/java/es/ujaen/dae/ujapack/entidades/EnvioTest.java
@@ -23,7 +23,7 @@ public class EnvioTest {
     }
     
     @Test
-    public void testCalculaImporte(){
+    public void testCalculaImporteTipo1(){
         
         Cliente cl1 = new Cliente("12345678A", "Paco", "Perez", "Calle falsa", "Jaén", "999000111", "email@email.com");
         Cliente cl2 = new Cliente("87654321B", "Maria", "Muñoz", "Calle verdadera", "Jaén", "555666777", "gmail@gmail.com");
@@ -40,6 +40,56 @@ public class EnvioTest {
         
         float importe = envio.getImporte();
         float result = 22.5f;
+        // Formula: importe = peso(kg) * dim(cm2) * (num_puntos_control + 1) / 1000
+        //assertEquals(importe, result);
+        Assertions.assertThat(importe).isEqualTo(result);
+        
+    }
+    
+    @Test
+    public void testCalculaImporteTipo2(){
+        
+        Cliente cl1 = new Cliente("12345678A", "Paco", "Perez", "Calle falsa", "Jaén", "999000111", "email@email.com");
+        Cliente cl2 = new Cliente("87654321B", "Maria", "Muñoz", "Calle verdadera", "Sevilla", "555666777", "gmail@gmail.com");
+        
+        List<Paquete> paquetes = new ArrayList<>();
+        paquetes.add(new Paquete(1.5f, 50.0f, 10.0f, 15.0f));
+        
+        Envio envio = new Envio(1234567890, cl1, cl2, paquetes);
+        // Envio tipo 1
+        List<PasoPuntoControl> listPPC = new ArrayList<>();
+        listPPC.add(new PasoPuntoControl(new Oficina("Jaén")));
+        listPPC.add(new PasoPuntoControl(new Oficina("Sevilla")));
+        envio.setRuta(listPPC);
+        envio.calculaImporte();
+        
+        float importe = envio.getImporte();
+        float result = 45.0f;
+        // Formula: importe = peso(kg) * dim(cm2) * (num_puntos_control + 1) / 1000
+        //assertEquals(importe, result);
+        Assertions.assertThat(importe).isEqualTo(result);
+        
+    }
+    
+    @Test
+    public void testCalculaImporteTipo3(){
+        
+        Cliente cl1 = new Cliente("12345678A", "Paco", "Perez", "Calle falsa", "Jaén", "999000111", "email@email.com");
+        Cliente cl2 = new Cliente("87654321B", "Maria", "Muñoz", "Calle verdadera", "Madrid", "555666777", "gmail@gmail.com");
+        
+        List<Paquete> paquetes = new ArrayList<>();
+        paquetes.add(new Paquete(1.5f, 50.0f, 10.0f, 15.0f));
+        
+        Envio envio = new Envio(1234567890, cl1, cl2, paquetes);
+        // Envio tipo 1
+        List<PasoPuntoControl> listPPC = new ArrayList<>();
+        listPPC.add(new PasoPuntoControl(new Oficina("Jaén")));
+        listPPC.add(new PasoPuntoControl(new Oficina("Madrid")));
+        envio.setRuta(listPPC);
+        envio.calculaImporte();
+        
+        float importe = envio.getImporte();
+        float result = 45.0f;
         // Formula: importe = peso(kg) * dim(cm2) * (num_puntos_control + 1) / 1000
         //assertEquals(importe, result);
         Assertions.assertThat(importe).isEqualTo(result);
