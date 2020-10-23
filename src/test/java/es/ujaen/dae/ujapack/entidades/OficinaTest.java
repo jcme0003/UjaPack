1a44ffb8a305fc14cacbf3e6ece5dc6b394f50f9
Ana <alag0006@red.ujaen.es>
2020-10-23 18:53:41 +0200

    Test Centro Logistica (sin terminar) y Oficina. Otras modificaciones (con Jose)

---
+++ src/test/java/es/ujaen/dae/ujapack/entidades/OficinaTest.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.ujaen.dae.ujapack.entidades;
+
+import es.ujaen.dae.ujapack.entidades.puntocontrol.Oficina;
+import java.util.Set;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author Ana
+ */
+public class OficinaTest {
+    public OficinaTest(){
+        
+    }
+    
+    @Test
+    void testValidacionOficina() {
+ 
+        Oficina of = new Oficina(
+                "74152369", 
+                "Jaén");
+
+        
+        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+        Set<ConstraintViolation<Oficina>> violations = validator.validate(of);
+                
+        Assertions.assertThat(violations).isEmpty();
+    }
+}
