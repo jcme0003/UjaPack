f538758d7723b56bba6784f5e72d6ce1ba24be3d
Jose Carlos Mena <jcme0003@red.ujaen.es>
2020-10-24 18:48:33 +0200

    Intento de algoritmo en profundidad

--- src/main/java/es/ujaen/dae/ujapack/app/UjaPackApp.java
+++ src/main/java/es/ujaen/dae/ujapack/app/UjaPackApp.java
@@ -7,9 +7,11 @@ package es.ujaen.dae.ujapack.app;
 
 import es.ujaen.dae.ujapack.entidades.Cliente;
 import es.ujaen.dae.ujapack.entidades.Envio;
+import es.ujaen.dae.ujapack.entidades.PasoPuntoControl;
 import es.ujaen.dae.ujapack.objetosvalor.Paquete;
 import es.ujaen.dae.ujapack.servicios.ServicioJSon;
 import es.ujaen.dae.ujapack.servicios.ServicioUjaPack;
+import es.ujaen.dae.ujapack.util.profundidad.Profundidad;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -30,12 +32,16 @@ public class UjaPackApp {
         SpringApplication servidor = new SpringApplication(UjaPackApp.class);
         ApplicationContext context = servidor.run(args);
         
-        ServicioUjaPack servicio = context.getBean(ServicioUjaPack.class);
-        
         ServicioJSon servicioJSon = new ServicioJSon();
         servicioJSon.cargaJSon("redujapack.json");
         servicioJSon.cargaConexiones("redujapack.json");
+        
+//        Profundidad profundidad = new Profundidad();
+//        profundidad.generaNodos(servicioJSon.getCentrosLogisticos());
+        
+        ServicioUjaPack servicio = context.getBean(ServicioUjaPack.class);
         servicio.setCentrosLogisticos(servicioJSon.getCentrosLogisticos());
+        
         
 //        servicio.cargaJSon("redujapack.json");
         servicio.listarCentrosLogisticos();
@@ -44,7 +50,7 @@ public class UjaPackApp {
 //        servicio.generaLocalizador();
 
         Cliente cl1 = new Cliente("12345678A", "Paco", "Perez", "Calle falsa", "Jaén", "999000111", "email@email.com");
-        Cliente cl2 = new Cliente("87654321B", "Maria", "Muñoz", "Calle verdadera", "Madrid", "555666777", "gmail@gmail.com");
+        Cliente cl2 = new Cliente("87654321B", "Maria", "Muñoz", "Calle verdadera", "Burgos", "555666777", "gmail@gmail.com");
         
         List<Paquete> paquetes = new ArrayList<>();
         paquetes.add(new Paquete(1.5f, 50.0f, 10.0f, 15.0f));
@@ -65,6 +71,11 @@ public class UjaPackApp {
             
             envio.getRuta().get(0).setFechaLlegada(LocalDate.now());
             envio.getRuta().get(0).setFechaSalida(LocalDate.now());
+            
+            for(PasoPuntoControl ppc : envio.getRuta()){
+                System.out.println(ppc.getPuntoDeControl().getIdCentro());
+            }
+            
             envio.actualizaEstadoEnvio();
             System.out.println(envio.getEstado());
         }
