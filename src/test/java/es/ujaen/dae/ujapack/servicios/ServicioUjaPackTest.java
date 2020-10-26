b1d40522ae2aeda546a4f06131761032d01f8ee4
Jose Carlos Mena <jcme0003@red.ujaen.es>
2020-10-26 01:44:24 +0100

    Imports innecesarios eliminados. Falta por hacer el algoritmo de busqueda en profundidad.

--- src/test/java/es/ujaen/dae/ujapack/servicios/ServicioUjaPackTest.java
+++ src/test/java/es/ujaen/dae/ujapack/servicios/ServicioUjaPackTest.java
@@ -174,45 +174,4 @@ public class ServicioUjaPackTest {
         
         Assertions.assertThat(!listado.isEmpty());
     }
-    
-    
-//    @Test
-//    public void testcalculaRuta(){
-//        List<Oficina> of1 = new ArrayList<Oficina>();
-//        of1.add(new Oficina("Santa Cruz de Tenerife"));
-//        
-//        List<Oficina> of2 = new ArrayList<Oficina>();
-//        of2.add(new Oficina("Alava"));
-//        
-//        List<CentroLogistico> centro = new ArrayList<CentroLogistico>();
-//        CentroLogistico pru1 = new CentroLogistico(10, "CL Noruega", "Santa Cruz de Brasil", of1, centro);
-//        centro.add(pru1);
-//        CentroLogistico pru2 = new CentroLogistico(7, "CL Pais Vasco-Cantabria", "Vitoria", of2, centro);
-//        centro.add(pru2);
-//        
-//        
-//       //Listar centros logisticos y saber si pru1 y pru2 existen dentro de esa lista        
-////        for(CentroLogistico centro2: centrosLogisticos.values()){
-////            if(servicioUjaPack.setCentrosLogisticos(centrosLogisticos))
-////        }
-//        //servicioUjaPack.calculaRuta("Santa Cruz de Tenerife", "Alava");
-//        
-//
-//        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
-//        Set<ConstraintViolation<CentroLogistico>> violations1 = validator.validate(pru1);
-//        Set<ConstraintViolation<CentroLogistico>> violations2 = validator.validate(pru2);        
-//        
-//        org.assertj.core.api.Assertions.assertThat(violations1).isEmpty();
-//        org.assertj.core.api.Assertions.assertThat(violations2).isEmpty();
-//            
-//            
-//        
-//        
-//        
-//        
-//    }
-    
-    
-    
-    
 }
