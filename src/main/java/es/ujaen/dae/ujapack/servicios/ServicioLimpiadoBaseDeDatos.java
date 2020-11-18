bba7eae05e39a450cda7cef31ae46d6ec1fb1a42
Ana <alag0006@red.ujaen.es>
2020-11-18 20:44:54 +0100

    Repositorios creados, servicio limpiar BD, y otras modificaciones

---
+++ src/main/java/es/ujaen/dae/ujapack/servicios/ServicioLimpiadoBaseDeDatos.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.ujaen.dae.ujapack.servicios;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.support.TransactionTemplate;
+
+/**
+ *
+ * @author Ana
+ */
+@Service
+public class ServicioLimpiadoBaseDeDatos {
+    @PersistenceContext
+    EntityManager em;
+    
+    @Autowired
+    TransactionTemplate transactionTemplate;
+    
+    /** 
+    * Lista de entidades a borrar.
+    */
+//    final String[] entidades = {
+//        "PuntoControl", 
+//        "Envio",
+//        "PasoPuntoControl",
+//        "CentroLogistico",
+//        "Oficina",
+//        "Cliente"            
+//    };
+    
+//    final String deleteFrom = "delete from ";
+//    
+//    /** Realizar borrado */
+//    void limpiar() {
+//        transactionTemplate.executeWithoutResult(transactionStatus -> {
+//            for (String tabla : entidades) {
+//                em.createQuery(deleteFrom + tabla).executeUpdate();
+//            }
+//        });
+//    }
+
+}
