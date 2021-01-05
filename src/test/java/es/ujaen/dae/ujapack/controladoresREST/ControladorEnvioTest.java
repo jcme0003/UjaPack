74a8f7b1d08aa4e110bb95e44727131f3f5a1da4
Jose Carlos Mena <jcme0003@red.ujaen.es>
2021-01-05 19:05:24 +0100

    crearRestTemplateBuilder OK. additionalMessageConverters hecho con Arrays.asList() - compatible con Java 8

--- src/test/java/es/ujaen/dae/ujapack/controladoresREST/ControladorEnvioTest.java
+++ src/test/java/es/ujaen/dae/ujapack/controladoresREST/ControladorEnvioTest.java
@@ -6,16 +6,19 @@
 package es.ujaen.dae.ujapack.controladoresREST;
 
 import es.ujaen.dae.ujapack.servicios.ServicioLimpiadoBaseDeDatos;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 /**
- *
- * @author joseo, Ana
+ * Test para controlador REST de envios
+ * @author joseo, Ana, Jose Carlos Mena
  */
 @SpringBootTest(classes = es.ujaen.dae.ujapack.app.UjaPackApp.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ControladorEnvioTest {
@@ -26,20 +29,19 @@ public class ControladorEnvioTest {
     @LocalServerPort
     int localPort;
     
-//    @Autowired
-//    MappingJacksonHttpMessageConverter springBootJacksonConverter;
+    @Autowired
+    MappingJackson2HttpMessageConverter springBootJacksonConverter;
     
     RestTemplateBuilder restTemplateBuilder;
     
-//    @PostConstruct
-//    void crearRestTemplateBuilder(){
-//        restTemplateBuilder = new RestTemplateBuilder()
-//                .rootUri("http://localhost:" + localPort + "/ujapack")
-//                .additionalMessageConverters(List.of(springBootJacksonConverter));
-//    }
+    /**
+     * Crear RestTemplate para las pruebas
+     */
+    @PostConstruct
+    void crearRestTemplateBuilder(){
+        restTemplateBuilder = new RestTemplateBuilder()
+                .rootUri("http://localhost:" + localPort + "/ujapack")
+                .additionalMessageConverters(Arrays.asList(springBootJacksonConverter));
+    }
     
-//    @BeforeEach
-//    void limpiarBaseDatos() {
-//        limpiadorBaseDatos.limpiar();
-//    }
 }
