90d63b971bfe76978e23a0890fa8009f516f38fe
Ana <alag0006@red.ujaen.es>
2021-01-05 13:44:40 +0100

    Error en el postConstruct,creación test puntos de control vacio

--- src/test/java/es/ujaen/dae/ujapack/controladoresREST/ControladorEnvioTest.java
+++ src/test/java/es/ujaen/dae/ujapack/controladoresREST/ControladorEnvioTest.java
@@ -5,13 +5,20 @@
  */
 package es.ujaen.dae.ujapack.controladoresREST;
 
+import es.ujaen.dae.ujapack.controladoresREST.DTO.DTOCliente;
 import es.ujaen.dae.ujapack.servicios.ServicioLimpiadoBaseDeDatos;
 import java.util.List;
 import javax.annotation.PostConstruct;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 /**
  *
@@ -26,18 +33,35 @@ public class ControladorEnvioTest {
     @LocalServerPort
     int localPort;
     
-//    @Autowired
-//    MappingJacksonHttpMessageConverter springBootJacksonConverter;
+    @Autowired
+    MappingJackson2HttpMessageConverter springBootJacksonConverter;
     
+    //TestRestTemplate restTemplateBuilder;
     RestTemplateBuilder restTemplateBuilder;
     
 //    @PostConstruct
-//    void crearRestTemplateBuilder(){
+//    void crearRestTemplateBuilder() {
 //        restTemplateBuilder = new RestTemplateBuilder()
-//                .rootUri("http://localhost:" + localPort + "/ujapack")
+//                .rootUri("http://localhost:" + localPort + "/ujacoin")
 //                .additionalMessageConverters(List.of(springBootJacksonConverter));
 //    }
+
+    @Test
+    public void testAltaClienteDuplicado() {
+        DTOCliente cliente = new DTOCliente(
+                            "87654321B",
+                            "Maria",
+                            "Muñoz",
+                            "Calle verdadera",
+                            "Sevilla",
+                            "555666777",
+                            "gmail@gmail.com");
+        TestRestTemplate restTemplate = new TestRestTemplate(restTemplateBuilder);
+        ResponseEntity<DTOCliente> respuesta = restTemplate.postForEntity("/clientes", cliente, DTOCliente.class);
+        
+        Assertions.assertThat(respuesta.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
     
+    }
 //    @BeforeEach
 //    void limpiarBaseDatos() {
 //        limpiadorBaseDatos.limpiar();
